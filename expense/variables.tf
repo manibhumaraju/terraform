b00f81462924f1dc2431cd4f9007c2439621097a
manibhumaraju <manishvarmab@gmail.com>
2024-09-14 12:41:02 +0530

    terraform

---
+++ expense/variables.tf
@@ -0,0 +1,21 @@
+variable "instance_names" {
+    type = list(string)
+    default = ["mysql", "backend", "frontend"]
+}
+
+variable "common_tags" {
+    type = map
+    default = {
+        Project = "expense"
+        Environment = "dev"
+        Terraform = "true"
+    }
+}
+
+variable "zone_id" {
+    default = "Z0960009J51UWVEUHO3A"
+}
+
+variable "domain_name" {
+    default = "manibhumaraju.online"
+}
